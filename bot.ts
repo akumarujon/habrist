dea6c5bd6c9b9f7ca37251a092e4d572befc3723
triistam <isakulovdev@gmail.com>
2023-08-22 09:35:56 +0500

    added better runner

--- bot.ts
+++ bot.ts
@@ -1,25 +1,14 @@
 import { Bot } from "./deps.ts";
 import "https://deno.land/x/dot_env@v0.1.0/load.ts";
+import { launch } from "./serve.ts"
 
 const TOKEN = Deno.env.get('TOKEN')
 const bot = new Bot(TOKEN);
+
 bot.command("start",async (ctx) => {
     await ctx.reply("A'zolik qo'shildi.")
 })
 
-import { serve, webhookCallback } from "./deps.ts";
+await launch();
 
-const handleUpdate = webhookCallback(bot, "std/http");
-
-serve(async (req) => {
-  if (req.method == "POST") {
-    try {
-      return await handleUpdate(req);
-    } catch (err) {
-      console.error(err);
-      return new Response();
-    }
-  }
-
-  return Response.redirect("https://t.me/habrist_bot", 302);
-});
+export { bot }
